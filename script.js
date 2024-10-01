d1c21f4e755780e3df747a7547caad7aac3f07b4
Rohin Sood <rs.rohinsood@gmail.com>
2024-10-01 10:11:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -52,10 +52,14 @@ setInterval(() => {
     if (currentLength !== previousLength) {
         console.log('SVG elements length changed:', currentLength);
         clickNextButton();
+        console.log('Clicked Next!');
         previousLength = currentLength;
         setTimeout(function(){ console.log("Waiting to Click Play") }, 2000);
-        const playButton = document.querySelector('.w-vulcan-v2-button.w-css-reset.w-css-reset-tree.w-css-reset-button-important');
+
+        const xpath = "/html/body/div[1]/div/div[4]/div[2]/main/div[4]/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[4]/div/div[3]/div/div/div/button";
+        const playButton = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         playButton.click();
+        
         console.log("Clicked Play!!!");
     }
 }, 1000);
