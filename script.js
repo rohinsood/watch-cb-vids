acd3c6c91b2ec03943140b5d49d9ec54facdf03a
Rohin Sood <rs.rohinsood@gmail.com>
2025-04-29 19:50:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -54,20 +54,65 @@ var checkAndMove = setInterval(() => {
         clickNextButton();
         console.log('Clicked Next!');
         previousLength = currentLength;
-        setTimeout(function(){ console.log("Waiting to Click Play") }, 4000);
+        setTimeout(function(){ console.log("Waiting to Click Play") }, 10000);
 
-        // click play button
-        
-        let playButton = document.querySelector("div.w-vulcan--background.w-css-reset")
-        if (playButton) {
-            playButton.click()
-            console.log("Clicked Play!!!");
-        } else {
-            document.querySelector("div.w-vulcan--background.w-css-reset").click()
-            console.log('document.querySelector("div.w-vulcan--background.w-css-reset") is null, trying again');
-        }   
+        playVideo();
                 
     }
 }, 1000);
 
+function playVideo() {
+    const playButton = document.querySelector('button.w-big-play-button');
+    if (playButton) {
+      playButton.style.pointerEvents = 'auto';
+      playButton.style.width = '1px';
+      playButton.style.height = '1px';
+      playButton.click();
+      setTimeout(() => {
+        playButton.style.pointerEvents = 'none';
+        playButton.style.width = '0px';
+        playButton.style.height = '0px';
+      }, 100);
+    } else {
+      console.error('Play Video button not found');
+    }
+
+    const settingsButton = document.querySelector('button[aria-label="Show settings menu"]');
+    if (settingsButton) {
+      settingsButton.style.pointerEvents = 'auto';
+      settingsButton.style.width = '1px';
+      settingsButton.style.height = '1px';
+      settingsButton.click();
+      setTimeout(() => {
+        settingsButton.style.pointerEvents = 'none';
+        settingsButton.style.width = '0px';
+        settingsButton.style.height = '0px';
+      }, 100);
+    } else {
+      console.error('Settings button not found');
+    }
+    
+    const speedButton = document.querySelector('button[data-handle="playbackRate"]');
+    if (speedButton) {
+      speedButton.style.pointerEvents = 'auto';
+      speedButton.style.width = '1px';
+      speedButton.style.height = '1px';
+      speedButton.click();
+      setTimeout(() => {
+        speedButton.style.pointerEvents = 'none';
+        speedButton.style.width = '0px';
+        speedButton.style.height = '0px';
+      }, 100);
+    } else {
+      console.error('Speed button not found');
+    }
+    
+    const speed2xInput = document.querySelector('input[type="radio"][name="Speed"][value="2x"]');
+    if (speed2xInput) {
+      speed2xInput.checked = true;
+      speed2xInput.dispatchEvent(new Event('change', { bubbles: true }));
+    } else {
+      console.error('2x speed input not found');
+    }
+}
 
